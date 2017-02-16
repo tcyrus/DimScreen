a89299af659c92c09f1ce260df3e04cfe292be96
tcyrus <github@tcyr.us>
2017-02-16 17:48:48 -0500

    Minor Upgrades

--- DimScreen/DimScreen/frmMain.cs
+++ DimScreen/DimScreen/frmMain.cs
@@ -2,111 +2,105 @@
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
-
-
 namespace DimScreen
 {
 
-    public partial class frmMain : Form
-    {
-        Timer timerPhase = new Timer() { Interval = 25, Enabled = false };
-        public System.Drawing.Rectangle Area { get; set; }
+	public partial class frmMain : Form
+	{
+		Timer timerPhase = new Timer() { Interval = 25, Enabled = false };
+		public System.Drawing.Rectangle Area { get; set; }
 
-        private float targetValue;
-        private float currentValue;
+		private float targetValue;
+		private float currentValue;
 
-        public float Dimness
-        {
-            get
-            {
-                return targetValue;
-            }
-            set
-            {
-                targetValue = value;
-                timerPhase.Start();
-            }
-        }
-        
+		public float Dimness
+		{
+			get
+			{
+				return targetValue;
+			}
+			set
+			{
+				targetValue = value;
+				timerPhase.Start();
+			}
+		}
+
 
 #region Enum
-        public enum GWL
-        {
-            ExStyle = -20
-        }
+		public enum GWL
+		{
+			ExStyle = -20
+		}
 
-        public enum WS_EX
-        {
-            Transparent = 0x20,
-            Layered = 0x80000
-        }
+		public enum WS_EX
+		{
+			Transparent = 0x20,
+			Layered = 0x80000
+		}
 
-        public enum LWA
-        {
-            ColorKey = 0x1,
-            Alpha = 0x2
-        }
+		public enum LWA
+		{
+			ColorKey = 0x1,
+			Alpha = 0x2
+		}
 #endregion
 
 #region DLLImport
-        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
-        public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
+		[DllImport("user32.dll", EntryPoint="GetWindowLong")]
+		public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
 
-        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
-        public static extern int SetWindowLong(IntPtr hWnd, GWL nIndex, int dwNewLong);
+		[DllImport("user32.dll", EntryPoint="SetWindowLong")]
+		public static extern int SetWindowLong(IntPtr hWnd, GWL nIndex, int dwNewLong);
 
-        [DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
-        public static extern bool SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte alpha, LWA dwFlags);
+		[DllImport("user32.dll", EntryPoint="SetLayeredWindowAttributes")]
+		public static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, LWA dwFlags);
 #endregion
 
 
-        protected override void OnShown(EventArgs e)
-        {
-            base.OnShown(e);
-        }
+		protected override void OnShown(EventArgs e)
+		{
+			base.OnShown(e);
+		}
 
+		public frmMain()
+		{
+			InitializeComponent();
+			timerPhase.Tick += timerPhase_Tick;
+		}
 
-        public frmMain()
-        {
-            InitializeComponent();
-            timerPhase.Tick += timerPhase_Tick;
-        }        
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// use working space rectangle info
+			this.Size = new System.Drawing.Size(Area.Width, Area.Height);
+			this.Location = new System.Drawing.Point(Area.X, Area.Y);
 
+			applyTransparency();
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // use working space rectangle info
-            this.Size = new System.Drawing.Size(Area.Width, Area.Height);
-            this.Location = new System.Drawing.Point(Area.X, Area.Y);
+		private void timerPhase_Tick(object sender, EventArgs e)
+		{
+			applyTransparency();
+		}
 
-            applyTransparency();
-        }
+		private void applyTransparency()
+		{
+			float calculatedValue = currentValue + Math.Sign(targetValue - currentValue) * 0.02f;
 
-        private void timerPhase_Tick(object sender, EventArgs e)
-        {
-            applyTransparency();
-        }
+			if (Math.Abs(targetValue - currentValue) < 0.02f * 2)
+			{
+				currentValue = targetValue;
+				timerPhase.Stop();
+			}
 
-        private void applyTransparency()
-        {
-            float calculatedValue = currentValue + Math.Sign(targetValue - currentValue) * 0.02f;
+			int wl = GetWindowLong(this.Handle, GWL.ExStyle);
+			wl = wl | 0x80000 | 0x20;
+			SetWindowLong(this.Handle, GWL.ExStyle, wl);
 
-            if (Math.Abs(targetValue - currentValue) < 0.02f * 2)
-            {
-                currentValue = targetValue;
-                timerPhase.Stop();
-            }
+			byte value = (byte)(calculatedValue * 255);
+			SetLayeredWindowAttributes(this.Handle, 0x128, value, LWA.Alpha);
 
-            int wl = GetWindowLong(this.Handle, GWL.ExStyle);
-            wl = wl | 0x80000 | 0x20;
-            SetWindowLong(this.Handle, GWL.ExStyle, wl);
-            
-            byte value = (byte)(calculatedValue * 255);
-            SetLayeredWindowAttributes(this.Handle, 0x128, value, LWA.Alpha);
-
-            currentValue = calculatedValue;
-        }
-
-
-    }
+			currentValue = calculatedValue;
+		}
+	}
 }
--- DimScreen/DimScreen/frmTray.cs
+++ DimScreen/DimScreen/frmTray.cs
@@ -3,145 +3,151 @@ using System.Windows.Forms;
 
 namespace DimScreen
 {
-    public partial class frmTray : Form
-    {
-        // list of overlays currently displayed
-        private System.Collections.Generic.List<frmMain> overlays = new System.Collections.Generic.List<frmMain>();
+	public partial class frmTray : Form
+	{
+		// list of overlays currently displayed
+		private System.Collections.Generic.List<frmMain> overlays = new System.Collections.Generic.List<frmMain>();
 
-        private ToolStripMenuItem lastMenuItem;
+		private ToolStripMenuItem lastMenuItem;
 
-        public frmTray()
-        {
-            InitializeComponent();
+		public frmTray()
+		{
+			InitializeComponent();
+		}
 
-        }
+		private void configureOverlays(float initialValue)
+		{
+			// remove exiting overlays
+			clearOverlays();
 
-        private void configureOverlays(float initialValue)
-        {
-            // remove exiting overlays
-            clearOverlays();
+			// add screens if they don't already exist
+			if (overlays.Count != Screen.AllScreens.Length)
+			{
+				// apply dimness onto all screens
+				foreach (var screen in Screen.AllScreens)
+				{
+					frmMain overlay = new frmMain();
+					overlay.Dimness = 0;
+					overlay.Area = screen.WorkingArea;
+					overlay.Show();
 
-            // add screens if they don't already exist
-            if (overlays.Count != Screen.AllScreens.Length)
-            {
-                // apply dimness onto all screens
-                foreach (var screen in Screen.AllScreens)
-                {
-                    frmMain overlay = new frmMain();
-                    overlay.Dimness = 0;
-                    overlay.Area = screen.WorkingArea;
-                    overlay.Show();
+					// add to list of overlays
+					overlays.Add(overlay);
+				}
+			}
 
-                    // add to list of overlays
-                    overlays.Add(overlay);
-                }
-            }
+			foreach (frmMain form in overlays)
+			{
+				form.Dimness = initialValue;
+			}
+		}
 
-            foreach (frmMain form in overlays)
-                form.Dimness = initialValue;
-        }
+		private void clearOverlays()
+		{
+			foreach (var form in overlays)
+			{
+				form.Dispose();
+			}
 
-        private void clearOverlays()
-        {
-            foreach (var form in overlays)
-                form.Dispose();
-
-            overlays.Clear();
-        }
-
-
-        private void frmTray_Load(object sender, EventArgs e)
-        {
-            menuNormal.Click += numericMenus_Click;
-            menu10.Click += numericMenus_Click;
-            menu20.Click += numericMenus_Click;
-            menu30.Click += numericMenus_Click;
-            menu40.Click += numericMenus_Click;
-            menu50.Click += numericMenus_Click;
-            menu60.Click += numericMenus_Click;
-            menu70.Click += numericMenus_Click;
-            menu80.Click += numericMenus_Click;
-            menu90.Click += numericMenus_Click;
-            menu100.Click += numericMenus_Click;
-
-            // get command line values
-            var arg = "";
-            var args = Environment.GetCommandLineArgs();
-            if (args.Length > 1) arg = args[1];
-
-            //TEST: force command line arg to test
-            //arg = "50";
-
-            if (arg != "")
-            {
-                try
-                {
-                    var val = float.Parse(arg) / 100;
-                    if (val > 100 || val < 0) throw new Exception("Out of range");
-                    configureOverlays(val);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Expecting number from 0 to 100 to represent percentage of dimming. 0 means no change, 100 being totally dark.");
-                    configureOverlays(0);
-                }
-            }
-            else
-            {
-                configureOverlays(0);
-            }
-        }
+			overlays.Clear();
+		}
 
 
-        private void menuExit_Click(object sender, EventArgs e)
-        {
-            //Display a Message box asking if the user wishes to exit.
-            DialogResult reply = MessageBox.Show("Are you sure you want to exit?", "Close?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+		private void frmTray_Load(object sender, EventArgs e)
+		{
+			menuNormal.Click += numericMenus_Click;
+			menu10.Click += numericMenus_Click;
+			menu20.Click += numericMenus_Click;
+			menu30.Click += numericMenus_Click;
+			menu40.Click += numericMenus_Click;
+			menu50.Click += numericMenus_Click;
+			menu60.Click += numericMenus_Click;
+			menu70.Click += numericMenus_Click;
+			menu80.Click += numericMenus_Click;
+			menu90.Click += numericMenus_Click;
+			menu100.Click += numericMenus_Click;
 
-            //If users answer was yes.
-            if (reply == DialogResult.Yes)
-            {
-                //Clean up any used memeory.
-                GC.Collect();
+			// get command line values
+			var args = Environment.GetCommandLineArgs();
+			var arg = (args.Length > 1) ? args[1] : string.Empty;
 
-                // remove all overlays
-                clearOverlays();
+			//TEST: force command line arg to test
+			//arg = "50";
 
-                //Remove tray Icon
-                notifyIcon1.Dispose();
+			if (arg.Length != 0)
+			{
+				try
+				{
+					var val = float.Parse(arg) / 100;
+					if (val > 100 || val < 0) throw new Exception("Out of range");
+					configureOverlays(val);
+				}
+				catch (Exception)
+				{
+					MessageBox.Show("Expecting number from 0 to 100 to represent percentage of dimming. 0 means no change, 100 being totally dark.");
+					configureOverlays(0);
+				}
+			}
+			else
+			{
+				configureOverlays(0);
+			}
+		}
 
-                //Close the Application.
-                this.Dispose();
-            }
-        }
 
-        private void numericMenus_Click(object sender, EventArgs e)
-        {
-            var menuItem = (ToolStripMenuItem)sender;
-            
-            if (lastMenuItem != null)
-                lastMenuItem.Checked = false;
+		private void menuExit_Click(object sender, EventArgs e)
+		{
+			//Display a Message box asking if the user wishes to exit.
+			DialogResult reply = MessageBox.Show("Are you sure you want to exit?", "Close?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-            menuItem.Checked = true;
+			//If users answer was yes.
+			if (reply == DialogResult.Yes)
+			{
+				//Clean up any used memeory.
+				GC.Collect();
 
-            lastMenuItem = menuItem;
+				// remove all overlays
+				clearOverlays();
 
-            var value = float.Parse((menuItem.Tag.ToString()));
+				//Remove tray Icon
+				notifyIcon1.Dispose();
 
-            foreach (frmMain form in overlays)
-                form.Dimness = value / 100;
-        }
+				//Close the Application.
+				this.Dispose();
+			}
+		}
 
-        private void menuRestart_Click(object sender, EventArgs e)
-        {
-            var exePath = Application.ExecutablePath;
-            System.Diagnostics.Process.Start(exePath, (overlays[0].Dimness * 100).ToString());
-            Application.Exit();
-        }
+		private void numericMenus_Click(object sender, EventArgs e)
+		{
+			var menuItem = (ToolStripMenuItem)sender;
 
-        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
-        {
-            contextMenuStrip1.Show();
-        }
-    }
+			if (lastMenuItem != null)
+			{
+				lastMenuItem.Checked = false;
+			}
+
+			menuItem.Checked = true;
+
+			lastMenuItem = menuItem;
+
+			var value = float.Parse(menuItem.Tag.ToString());
+
+			foreach (frmMain form in overlays)
+			{
+				form.Dimness = value / 100;
+			}   
+		}
+
+		private void menuRestart_Click(object sender, EventArgs e)
+		{
+			var exePath = Application.ExecutablePath;
+			System.Diagnostics.Process.Start(exePath, (overlays[0].Dimness * 100).ToString());
+			Application.Exit();
+		}
+
+		private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+		{
+			contextMenuStrip1.Show();
+		}
+	}
 }
